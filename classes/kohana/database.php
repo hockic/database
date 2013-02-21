d0c6f3d5091e3ba5c1fdbf17d493f3187e0bfba0
q7eb2a <q7eb2a@gmail.com>
2013-02-21 04:14:44 +0100

    Update classes/kohana/database.php

--- classes/kohana/database.php
+++ classes/kohana/database.php
@@ -9,13 +9,13 @@
  * @license http://www.opensource.org/licenses/isc-license.txt
  */
 class Kohana_Database extends PDO {
-	
+
 	// Config
 	protected $config;
-	
+
 	// Instances
 	protected static $_instance;
-	
+
 	/**
 	 * Singleton instance
 	 * 
@@ -27,31 +27,31 @@ class Kohana_Database extends PDO {
 		{
 			DB::$_instance = new DB();
 		}
-		
+
 		return DB::$_instance;
 	}
-	
+
 	public function __construct()
 	{
 		// Load database config
-		$this->config = Kohana::$config->load('database.pdo');
-		
+		$this->config = Kohana::$config->load('database');
+
 		// Connect to the database
-		parent::__construct($this->config['dsn'] , $this->config['username'], $this->config['password']);
-		
+		parent::__construct($this->config->dsn , $this->config->username, $this->config->password);
+
 		// Force PDO to use exceptions for all errors
 		parent::setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-		
+
 		// Set fetch mode
-		parent::setAttribute( PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ );
-		
+		parent::setAttribute( PDO::ATTR_DEFAULT_FETCH_MODE, $this->config->fetch_mode );
+
 		if( $this->config['persistent'] !== FALSE )
 		{
 			// Make the connection persistent
 			parent::setAttribute( PDO::ATTR_PERSISTENT, TRUE );
 		}
 	}
-	
+
 	/**
 	 * Enables the query to be cached for a specified amount of time.
 	 * 
@@ -66,23 +66,23 @@ class Kohana_Database extends PDO {
 		// Set the cache key based on the database instance name and SQL
 		$cache_key = 'DB::cached('.serialize(array($sql,$params,$multiple,$lifetime)).')';
 		
-		$result = Kohana::cache($cache_key, NULL, $lifetime);
-		
 		// Try to get from cache
+		$result = Kohana::cache($cache_key, NULL, $lifetime);
+
 		if( $result == NULL )
 		{
 			// Get from database
 			$stmt = parent::prepare($sql);
 			$stmt->execute($params);
 			$result = ( $multiple ) ? $stmt->fetchAll() : $stmt->fetch();
-			
-			if(Kohana::$caching)
+
+			if( Kohana::$caching )
 			{
 				// Cache the result
 				Kohana::cache($cache_key, $result, $lifetime);
 			}
 		}
-		
+
 		return $result;
 	}
 }
