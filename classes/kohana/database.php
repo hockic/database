54ffbc7fc52936ad71f894ab8c0e05ce69abd447
q7eb2a <q7eb2a@gmail.com>
2013-07-12 01:46:29 +0200

    Update database.php

--- classes/kohana/database.php
+++ classes/kohana/database.php
@@ -67,6 +67,137 @@ class Kohana_Database extends PDO {
 	}
 
 	/**
+	 * Shorhand method for INSERT statements
+	 * 
+	 * [!] This method is not tested so be careful
+	 * 
+	 * @param   string     $table       table to insert into
+	 * @param   array      $parameters  list of column names and values
+	 */
+	public function insert($table, array $parameters)
+	{
+		// Join keys
+		$keys = implode(',', array_keys($parameters));
+		
+		// Prepend array keys with a colon
+		$value_keys = ':' . implode(',:', array_keys($parameters));
+		
+		// Start a insertion query
+		$query = "INSERT INTO " . $table . " (" . $keys . ") VALUES (" . $value_keys . ")";
+
+		// Initiate array for the final parameters
+		$final_parameters = array();
+
+		foreach($parameters as $key => $value)
+		{
+			// Prefix param keys with colons
+			$final_parameters[':'.$key] = $value;
+		}
+
+		// Prepare the statement
+		$stmt = parent::prepare($query);
+
+		// Execute prepared statement
+		$stmt->execute($final_parameters);
+
+		return $stmt;
+	}
+
+	/**
+	 * Shorhand method for UPDATE statements
+	 * 
+	 * [!] This method is not tested so be careful
+	 * 
+	 * @param   string     $table       table name
+	 * @param   array      $parameters  list of column names and values
+	 * @param   array      $where       where clause keys
+	 */
+	public function update($table, array $parameters, array $where = NULL)
+	{
+		// Start a deletion query
+		$query = "UPDATE " . $table . " SET ";
+
+		foreach($parameters as $key => $value)
+		{
+			// Generate column list
+			$query .= $key . " = :" . $key;
+			
+			end($parameters);
+			$query .= ($key === key($parameters)) ? " " : ", ";
+		}
+
+		// Generate where clause
+		$query .= "WHERE 1";
+
+		if($where !== NULL)
+		{
+			foreach($where as $key => $value)
+			{
+				// Add deletion conditions
+				$query .= " AND " . $key . " = :where_".$key;
+			}
+			
+			// Initiate array for the final parameters
+			$final_where_parameters = array();
+	
+			foreach($where as $key => $value)
+			{
+				// Prefix param keys with colons
+				$final_where_parameters[':where_'.$key] = $value;
+			}
+			
+			$parameters = $parameters + $final_where_parameters;
+		}
+
+
+		// Prepare the statement
+		$stmt = parent::prepare($query);
+
+		// Execute prepared statement
+		$stmt->execute($parameters);
+
+		return $stmt;
+	}
+
+	/**
+	 * Shorhand method for DELETE statements
+	 * 
+	 * [!] This method is not tested so be careful
+	 * 
+	 * @param   string       $table        table to delete from
+	 * @param   array        $conditions   deletion conditions
+	 * @param   int|boolean  $limit        row limit
+	 */
+	public function delete($table, array $conditions = NULL, $limit = 1)
+	{
+		// Start a deletion query
+		$query = "DELETE FROM " . $table . " WHERE 1";
+
+		if($conditions !== NULL)
+		{
+			foreach($conditions as $key => $value)
+			{
+				// Add deletion conditions
+				$query .= " AND " . $key . " = :".$key;
+			}
+		}
+		
+		if($limit !== FALSE)
+		{
+			// Add limiting
+			$query .= " LIMIT " . $limit;
+		}
+
+		// Prepare the statement
+		$stmt = parent::prepare($query);
+
+		// Execute prepared statement
+		$stmt->execute($conditions);
+
+		return $stmt;
+	}
+	
+	/**
 	 * Enables the query to be cached for a specified amount of time.
 	 * 
 	 * @param   string   $sql       query string
